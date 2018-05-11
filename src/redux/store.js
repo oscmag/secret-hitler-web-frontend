8fca1f213761b2390da2f79f6c17afb9481580a9
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-11 11:32:49 +0200

    feat: persist redux store to local storage on app exit

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+
+import reducers from './reducers';
+import backendCall from './middlewares/backendCall';
+import {loadState, saveState} from './localStorage';
+
+const persistedState = loadState();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, persistedState, composeEnhancers(applyMiddleware(backendCall)));
+
+store.subscribe(() => saveState(store.getState()));
+
+export default store;
