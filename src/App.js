2ba228851c2ea90019791aafed612378f76c5203
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-12 12:29:44 +0200

    feat: landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import LandingPage from './Pages/Landing';
 import './App.css';
 
 export default class App extends React.Component {
@@ -9,6 +10,7 @@ export default class App extends React.Component {
       <Router>
         <div id='app'>
           <Switch>
+            <Route exact path='/' component={LandingPage}/>
           </Switch>
       </div>
       </Router>
