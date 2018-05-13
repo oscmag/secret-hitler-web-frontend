de9085800ff5bca4e57db8e4dcadc2eb8c7fac26
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-13 16:47:07 +0200

    chore: disable redux store persisting to local storage for dev mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducers from './reducers';
 import backendCall from './socket.middleware';
-import { loadState, saveState } from './localStorage';
+// import { loadState, saveState } from './localStorage';
 
-const persistedState = loadState();
+// const persistedState = loadState();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, persistedState, composeEnhancers(applyMiddleware(backendCall)));
+const store = createStore(reducers, /*persistedState,*/ composeEnhancers(applyMiddleware(backendCall)));
 
-store.subscribe(() => saveState(store.getState()));
+// store.subscribe(() => saveState(store.getState()));
 
 export default store;
