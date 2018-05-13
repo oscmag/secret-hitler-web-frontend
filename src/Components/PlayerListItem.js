3e7d9c525095aeed65be5e7b229a0660af383d76
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-13 21:13:25 +0200

    feat: add games played/won to PlayerListItem + CSS classes array

--- src/Components/PlayerListItem.js
+++ src/Components/PlayerListItem.js
@@ -3,15 +3,21 @@ import React from 'react';
 import './PlayerListItem.css';
 
 export default (props) => {
-  const { user } = props.player;
+  const { player, player: { user } } = props;
+  const classes = [
+    player.president ? 'president' : null,
+    player.chancellor ? 'chancellor' : null,
+    player.executed ? 'executed' : null,
+  ];
   return (
     user && <div className='player-list-item'>
       <img src={`./assets/avatars/${user.avatar}.png`} alt='user avatar'/>
-      <div>
+      <div className={classes.join(' ')}>
         <h2>{user.name}</h2>
-        <p><span>Games played</span> {user.games.played}</p>
-        <p><span>Games won</span> {user.games.won}</p>
-        <p><span>Ratio</span> {(user.games.won/user.games.played).toFixed(2)}</p>
+        {user.games.played && <p><span>Games played</span> {user.games.played}</p>}
+        {user.games.won && <p><span>Games won</span> {user.games.won}</p>}
+        {user.games.played && user.games.won &&
+          <p><span>Ratio</span> {(user.games.won/user.games.played).toFixed(2)}</p>}
       </div>
     </div>
   );
