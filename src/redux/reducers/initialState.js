dcc3a9e39c682041150edd80fe839635349c092b
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-17 17:21:12 +0200

    chore: rename initialState

---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,20 @@
+import { user, game} from './mocks';
+
+const initUser = true;
+const initGame = false;
+
+export default {
+  app: {
+    gameId: 'xwfKQKPk0kK_tpxPAABy',
+    serverAlert: '',
+    serverError: '',
+    modals: {
+      rules: false,
+      settings: false,
+      showRoles: false,
+      showFascists: false,
+    }
+  },
+  game: initGame ? game : {},
+  user: initUser ? user : {},
+};
