2610b70d6c3635732360bb3efedbcaeeb233de9c
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-18 09:43:51 +0200

    feat: make PlayerList more defensive

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -4,12 +4,13 @@ import PlayerListItem from './PlayerListItem';
 import './PlayerList.css';
 
 export default (props) => {
+  if (!props.playerList) return null;
   return (
     props.playerList.length === 0 ?
     <div className='empty-list'><h2>No players to display.</h2></div> :
     <div className='player-list'>
       {props.playerList.map(player =>
-        <PlayerListItem key={player.user.id} player={player}/>
+        <PlayerListItem key={player.user.id} player={player} {...props}/>
       )}
     </div>
   );
