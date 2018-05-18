ed3e2e4cfd836c0651612c4a33ae2521dd5563b0
Janosh Riebesell <janosh.riebesell@gmail.com>
2018-05-18 09:41:36 +0200

    feat: add redux-notifications to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Notifs } from 'redux-notifications';
 
 import App from './App';
 import store from './redux/store';
@@ -9,7 +10,10 @@ import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <React.Fragment>
+      <App/>
+      <Notifs/>
+    </React.Fragment>
   </Provider>,
   document.getElementById('root')
 );
